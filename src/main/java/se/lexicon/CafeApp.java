090baa1aa925e30bae6c36d19159e3f8b131a07f
Agnes Timar <timaragnes@outlook.hu>
2026-06-10 11:30:36 +0200

    Display the café menu with item numbers, names, and prices

--- src/main/java/se/lexicon/CafeApp.java
+++ src/main/java/se/lexicon/CafeApp.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CafeApp {
     static void main() {
         String customer = greetCustomer();
+        displayMenu();
     }
 
     public static String greetCustomer() {
@@ -16,5 +17,32 @@ public class CafeApp {
 
         scanner.close();
         return customerName;
+    }
+
+    public static void displayMenu() {
+        String item1 = "Espresso";
+        double price1 = 25.00;
+
+        String item2 = "Cappuccino";
+        double price2 = 35.00;
+
+        String item3 = "Latte";
+        double price3 = 40.00;
+
+        String item4 = "Croissant";
+        double price4 = 30.00;
+
+        String item5 = "Sandwich";
+        double price5 = 55.00;
+
+        IO.println("==============================");
+        IO.println("         Lexicon Cafe");
+        IO.println("==============================");
+        System.out.printf("%-1d. %-15s %6.2f SEK%n",1, item1, price1);
+        System.out.printf("%-1d. %-15s %6.2f SEK%n",2, item2, price2);
+        System.out.printf("%-1d. %-15s %6.2f SEK%n",3, item3, price3);
+        System.out.printf("%-1d. %-15s %6.2f SEK%n",4, item4, price4);
+        System.out.printf("%-1d. %-15s %6.2f SEK%n",5, item5, price5);
+        IO.println("==============================");
     }
 }
